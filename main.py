325c0c998d0196c88d4b26a340318f4ed1782dd8
Matthew Hillman <matthewahillman@pm.me>
2021-04-07 12:31:39 +0100

    Adding in an option to not have special characters

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import secrets
 import string
 
-alphabet = string.ascii_letters + string.digits + string.punctuation
-password = secrets.choice(string.ascii_lowercase)
-password += secrets.choice(string.ascii_uppercase)
-password += secrets.choice(string.digits)
-password += secrets.choice(string.punctuation)
+
 password_length = int(input("Password length? "))
 random = secrets.randbelow(10)
+special_chars = input("Special characters? Y/N ").upper()
+if special_chars == 'Y':
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = secrets.choice(string.ascii_lowercase)
+    password += secrets.choice(string.ascii_uppercase)
+    password += secrets.choice(string.digits)
+    password += secrets.choice(string.punctuation)
+else:
+    alphabet = string.ascii_letters + string.digits
+    password = secrets.choice(string.ascii_lowercase)
+    password += secrets.choice(string.ascii_uppercase)
+    password += secrets.choice(string.digits)
+
 while True:
     password = ''.join(secrets.choice(alphabet) for i in range(password_length))
     if (any(c.islower() for c in password)
